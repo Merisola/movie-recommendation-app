135707202f28a4630944326423d0e43ed17be9f7
Meron Solomon <meronsolomondana@gmail.com>
2025-07-31 23:44:29 +0300

    feat: integrate movie API to fetch trending and searched movies

---
+++ services/movieService.ts
@@ -0,0 +1,27 @@
+// /services/movieService.ts
+import axios from "axios";
+
+const BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+
+export const getTrendingMovies = async () => {
+  try {
+    const res = await axios.get(`${BASE_URL}/trending/movie/week`, {
+      params: { api_key: API_KEY },
+    });
+    return res.data.results;
+  } catch (error) {
+    throw new Error("Failed to fetch trending movies");
+  }
+};
+
+export const searchMovies = async (query: string) => {
+  try {
+    const res = await axios.get(`${BASE_URL}/search/movie`, {
+      params: { api_key: API_KEY, query },
+    });
+    return res.data.results;
+  } catch (error) {
+    throw new Error("Search failed");
+  }
+};
